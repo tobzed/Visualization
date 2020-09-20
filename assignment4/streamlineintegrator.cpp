30f2b058bb9b27176f80c72f246c56fce61307c0
fred <tobyedwards@hotmail.co.uk>
2020-09-20 19:10:19 +0200

    start task 2

---
+++ assignment4/streamlineintegrator.cpp
@@ -0,0 +1,161 @@
+/*********************************************************************
+ *  Author  : Himangshu Saikia
+ *  Init    : Tuesday, September 19, 2017 - 15:08:33
+ *
+ *  Project : KTH Inviwo Modules
+ *
+ *  License : Follows the Inviwo BSD license model
+ *********************************************************************
+ */
+
+#include <inviwo/core/interaction/events/mouseevent.h>
+#include <inviwo/core/util/utilities.h>
+#include <labstreamlines/integrator.h>
+#include <labstreamlines/streamlineintegrator.h>
+#include <labutils/scalarvectorfield.h>
+
+namespace inviwo {
+
+// The Class Identifier has to be globally unique. Use a reverse DNS naming
+// scheme
+const ProcessorInfo StreamlineIntegrator::processorInfo_{
+    "org.inviwo.StreamlineIntegrator",  // Class identifier
+    "Streamline Integrator",            // Display name
+    "KTH Lab",                          // Category
+    CodeState::Experimental,            // Code state
+    Tags::None,                         // Tags
+};
+
+const ProcessorInfo StreamlineIntegrator::getProcessorInfo() const { return processorInfo_; }
+
+StreamlineIntegrator::StreamlineIntegrator()
+    : Processor()
+    , inData("volIn")
+    , meshOut("meshOut")
+    , meshBBoxOut("meshBBoxOut")
+    , propStartPoint("startPoint", "Start Point", vec2(0.5f, 0.5f), vec2(-1.f), vec2(1.f), vec2(0.1))
+    , propSeedMode("seedMode", "Seeds")
+    , propNumStepsTaken("numstepstaken", "Number of actual steps", 0, 0, 100000)
+    , mouseMoveStart("mouseMoveStart", "Move Start", [this](Event* e) { eventMoveStart(e); },
+                     MouseButton::Left, MouseState::Press | MouseState::Move)
+// TODO: Initialize additional properties
+// propertyName("propertyIdentifier", "Display Name of the Propery",
+// default value (optional), minimum value (optional), maximum value (optional),
+// increment (optional)); propertyIdentifier cannot have spaces
+    , backwardProp("backwardIntegration", "Backward integration", false)
+    , propNumSteps("numSteps", "Number of steps", 50, 1, 300)
+    , propStepSize("stepSize", "Step size", 0.5, 0, 1)
+
+{
+    // Register Ports
+    addPort(inData);
+    addPort(meshOut);
+    addPort(meshBBoxOut);
+
+    // Register Properties
+    propSeedMode.addOption("one", "Single Start Point", 0);
+    propSeedMode.addOption("multiple", "Multiple Seeds", 1);
+    addProperty(propSeedMode);
+    addProperty(propStartPoint);
+    addProperty(propNumStepsTaken);
+    propNumStepsTaken.setReadOnly(true);
+    propNumStepsTaken.setSemantics(PropertySemantics::Text);
+    addProperty(mouseMoveStart);
+
+    // TODO: Register additional properties
+    // addProperty(propertyName);
+    addProperty(backwardProp);
+    addProperty(propNumSteps);
+    addProperty(propStepSize);
+    // Show properties for a single seed and hide properties for multiple seeds
+    // (TODO)
+    propSeedMode.onChange([this]() {
+        if (propSeedMode.get() == 0) {
+            util::show(propStartPoint, mouseMoveStart, propNumStepsTaken);
+            // util::hide(...)
+        } else {
+            util::hide(propStartPoint, mouseMoveStart, propNumStepsTaken);
+            // util::show(...)
+        }
+    });
+}
+
+void StreamlineIntegrator::eventMoveStart(Event* event) {
+    if (!inData.hasData()) return;
+    auto mouseEvent = static_cast<MouseEvent*>(event);
+    vec2 mousePos = mouseEvent->posNormalized();
+
+    // Map to bounding box range
+    mousePos[0] *= static_cast<float>(BBoxMax_[0] - BBoxMin_[0]);
+    mousePos[1] *= static_cast<float>(BBoxMax_[1] - BBoxMin_[1]);
+    mousePos += static_cast<vec2>(BBoxMin_);
+
+    // Update starting point
+    propStartPoint.set(mousePos);
+    event->markAsUsed();
+}
+
+void StreamlineIntegrator::process() {
+    // Get input
+    if (!inData.hasData()) {
+        return;
+    }
+    auto vol = inData.getData();
+
+    // Retreive data in a form that we can access it
+    auto vectorField = VectorField2::createFieldFromVolume(vol);
+    BBoxMin_ = vectorField.getBBoxMin();
+    BBoxMax_ = vectorField.getBBoxMax();
+
+    auto bboxMesh = std::make_shared<BasicMesh>();
+    std::vector<BasicMesh::Vertex> bboxVertices;
+
+    // Make bounding box without vertex duplication, instead of line segments which duplicate
+    // vertices, create line segments between each added points with connectivity type of the index
+    // buffer
+    auto indexBufferBBox = bboxMesh->addIndexBuffer(DrawType::Lines, ConnectivityType::Strip);
+    // Bounding Box vertex 0
+    vec4 black = vec4(0, 0, 0, 1);
+    Integrator::drawNextPointInPolyline(BBoxMin_, black, indexBufferBBox.get(), bboxVertices);
+    Integrator::drawNextPointInPolyline(vec2(BBoxMin_[0], BBoxMax_[1]), black,
+                                        indexBufferBBox.get(), bboxVertices);
+    Integrator::drawNextPointInPolyline(BBoxMax_, black, indexBufferBBox.get(), bboxVertices);
+    Integrator::drawNextPointInPolyline(vec2(BBoxMax_[0], BBoxMin_[1]), black,
+                                        indexBufferBBox.get(), bboxVertices);
+    // Connect back to the first point, to make a full rectangle
+    indexBufferBBox->add(static_cast<std::uint32_t>(0));
+    bboxMesh->addVertices(bboxVertices);
+    meshBBoxOut.setData(bboxMesh);
+
+    auto mesh = std::make_shared<BasicMesh>();
+    std::vector<BasicMesh::Vertex> vertices;
+
+    if (propSeedMode.get() == 0) {
+        auto indexBufferPoints = mesh->addIndexBuffer(DrawType::Points, ConnectivityType::None);
+        auto indexBufferPolyLine = mesh->addIndexBuffer(DrawType::Lines, ConnectivityType::Strip);
+        vec2 startPoint = propStartPoint.get();
+        // Draw start point
+        Integrator::drawPoint(startPoint, vec4(0, 0, 0, 1), indexBufferPoints.get(), vertices);
+
+        // TODO: Create one stream line from the given start point
+        vec2 currentPoint = startPoint;
+        for (int i = 0; i < propNumSteps.get(); i++) {
+            currentPoint = Integrator::RK4( vectorField, currentPoint, propStepSize.get(), backwardProp.get() ? -1.0 : 1.0 );
+            Integrator::drawPoint(currentPoint, black, indexBufferPoints.get(), vertices);
+            Integrator::drawNextPointInPolyline(currentPoint, black, indexBufferPolyLine.get(), vertices);
+        }
+        // TODO: Use the propNumStepsTaken property to show how many steps have actually been
+        // integrated This could be different from the desired number of steps due to stopping
+        // conditions (too slow, boundary, ...)
+        propNumStepsTaken.set(0);
+
+    } else {
+        // TODO: Seed multiple stream lines either randomly or using a uniform grid
+        // (TODO: Bonus, sample randomly according to magnitude of the vector field)
+    }
+
+    mesh->addVertices(vertices);
+    meshOut.setData(mesh);
+}  // namespace inviwo
+
+}  // namespace inviwo
---
+++ assignment4/streamlineintegrator.h
@@ -0,0 +1,116 @@
+/*********************************************************************
+ *  Author  : Himangshu Saikia
+ *  Init    : Tuesday, September 19, 2017 - 15:08:33
+ *
+ *  Project : KTH Inviwo Modules
+ *
+ *  License : Follows the Inviwo BSD license model
+ *********************************************************************
+ */
+
+#pragma once
+
+#include <inviwo/core/common/inviwo.h>
+#include <inviwo/core/datastructures/geometry/basicmesh.h>
+#include <inviwo/core/datastructures/volume/volumeram.h>
+#include <inviwo/core/ports/meshport.h>
+#include <inviwo/core/ports/volumeport.h>
+#include <inviwo/core/processors/processor.h>
+#include <inviwo/core/properties/boolproperty.h>
+#include <inviwo/core/properties/compositeproperty.h>
+#include <inviwo/core/properties/eventproperty.h>
+#include <inviwo/core/properties/optionproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <labstreamlines/labstreamlinesmoduledefine.h>
+#include <labutils/scalarvectorfield.h>
+
+namespace inviwo {
+
+/** \docpage{org.inviwo.StreamlineIntegrator, Streamline Integrator}
+    ![](org.inviwo.StreamlineIntegrator.png?classIdentifier=org.inviwo.StreamlineIntegrator)
+
+    Processor to integrate streamlines.
+
+    ### Inports
+    * __data__ The input here is 2-dimensional vector field (with vectors of
+    two components thus two values within each voxel) but it is represented
+    by a 3-dimensional volume.
+    This processor deals with 2-dimensional data only, therefore it is assumed
+    the z-dimension will have size 1 otherwise the 0th slice of the volume
+    will be processed.
+
+    ### Outports
+    * __meshout__ The output mesh contains linesegments making up either a single or
+    multiple stream lines
+    * __meshBBoxOut__ Mesh with boundling box
+
+    ### Properties
+    * __propSeedMode__ Mode for the number of seeds, either a single start point
+   or multiple
+    * __propStartPoint__ Location of the start point
+    * __mouseMoveStart__ Move the start point when a selected mouse button is
+    * __numStepsTaken__ Number of steps actually taken for a single streamline
+   pressed (default left)
+*/
+
+class IVW_MODULE_LABSTREAMLINES_API StreamlineIntegrator : public Processor {
+    // Construction / Deconstruction
+public:
+    StreamlineIntegrator();
+    virtual ~StreamlineIntegrator() = default;
+
+    // Methods
+public:
+    virtual const ProcessorInfo getProcessorInfo() const override;
+    static const ProcessorInfo processorInfo_;
+
+protected:
+    /// Our main computation function
+    virtual void process() override;
+
+    /// Function to handle mouse interaction for a single streamline
+    void eventMoveStart(Event* event);
+
+    // (TODO: You could define some helper functions here,
+    // e.g. a function creating a single streamline from one seed point)
+
+    // Ports
+public:
+    // Input Vector Field
+    VolumeInport inData;
+
+    // Output mesh
+    MeshOutport meshOut;
+
+    // Output mesh for bounding box and gridlines
+    MeshOutport meshBBoxOut;
+
+    // Properties
+public:
+
+    FloatVec2Property propStartPoint;
+    TemplateOptionProperty<int> propSeedMode;
+
+    IntProperty propNumStepsTaken;
+    EventProperty mouseMoveStart;
+
+    // TODO: Declare additional properties
+    // Some types that you might need are given below
+    // IntProperty properyName;
+    // FloatProperty propertyName2;
+    // IntVec2Property propertyName3;
+    // TemplateOptionProperty<int> propertyName4;
+    // BoolProperty propertyName4;
+
+    BoolProperty backwardProp;
+    IntProperty propNumSteps;
+    DoubleProperty propStepSize;
+
+    // Attributes
+private:
+    dvec2 BBoxMin_{0, 0};
+    dvec2 BBoxMax_{0, 0};
+};
+
+}  // namespace inviwo
